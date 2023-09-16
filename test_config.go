ea1f14da9a39714be6d6ce69364c0470b8ee2787
fmhr <fmhr29@gmail.com>
2023-09-16 14:55:56 +0900

    add test

---
+++ test_config.go
@@ -0,0 +1,25 @@
+package fj
+
+import (
+	"testing"
+)
+
+func TestCheckConfigFile(t *testing.T) {
+	// 1. Cmdが空の場合
+	t.Run("empty cmd", func(t *testing.T) {
+		cnf := &config{Cmd: ""}
+		err := checkConfigFile(cnf)
+		if err == nil {
+			t.Errorf("expected error but got nil")
+		}
+	})
+
+	// 2. Cmdに値が設定されている場合
+	t.Run("valid cmd", func(t *testing.T) {
+		cnf := &config{Cmd: "sample command"}
+		err := checkConfigFile(cnf)
+		if err != nil {
+			t.Errorf("expected nil but got error: %v", err)
+		}
+	})
+}
