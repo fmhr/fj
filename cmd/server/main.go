464aba6c599c9dae8e71c2ba0adc36854eaf5732
fmhr <fmhr29@gmail.com>
2023-09-22 07:40:00 +0900

    add handler

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,16 +1,73 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/fmhr/fj"
 )
 
 func main() {
 	log.Println("starting server...")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	log.Println("listening on port " + port)
 	http.HandleFunc("/", handler)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	log.Println("handling request...")
-	w.Write([]byte("hello"))
+	// seed
+	seedString := r.URL.Query().Get("seed")
+	if seedString == "" {
+		http.Error(w, "no seed specified", http.StatusBadRequest)
+		return
+	}
+	// seed
+	seed, err := strconv.Atoi(seedString)
+	if err != nil {
+		http.Error(w, "invalid seed specified", http.StatusBadRequest)
+		return
+	}
+	log.Println("seed:", seed)
+
+	var config fj.Config
+	config.GenPath = "gen"
+	config.VisPath = "vis"
+	config.TesterPath = "tester"
+	reactiveString := r.URL.Query().Get("reactive")
+	if reactiveString == "" {
+		config.Reactive = false
+	} else {
+		config.Reactive = true
+	}
+	// GEN
+	fj.Gen(&config, seed)
+	// RUN
+	rtn, err := run(&config, seed)
+	if err != nil {
+		log.Println("run error:", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	jsonData, err := json.Marshal(rtn)
+	if err != nil {
+		log.Println("json marshal error:", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsonData)
+}
+
+func run(cfg *fj.Config, seed int) (map[string]float64, error) {
+	if cfg.Reactive {
+		return fj.ReactiveRun(cfg, seed)
+	}
+	return fj.RunVis(cfg, seed)
 }
