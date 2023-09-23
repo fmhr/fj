dd2ffe916c668efa0ad2e5e980cd2ffac208470c
fmhr <fmhr29@gmail.com>
2023-09-23 13:23:32 +0900

    fix test

--- normalRun_test.go
+++ normalRun_test.go
@@ -8,21 +8,21 @@ import (
 func TestRun(t *testing.T) {
 	// 必要なフォルダの作成
 	if _, err := os.Stat("testdata"); err != nil {
-		if err := os.Mkdir("testdata", 0755); err != nil {
+		if err := os.Mkdir("testdata", 0777); err != nil {
 			t.Fatalf("failed to create testdata directory: %v", err)
 		}
 	}
 	// テストの設定
 	cnf := &Config{
 		Cmd:         "cat",
-		InfilePath:  "testdata/input/",
-		OutfilePath: "testdata/output/",
+		InfilePath:  "testdata/",
+		OutfilePath: "testdata/",
 	}
 	seed := 1
 
 	// 入力ファイルの作成
 	inputConten := "Hello, World!\n"
-	err := os.WriteFile("testdata/input/0001.txt", []byte(inputConten), 0644)
+	err := os.WriteFile("testdata/0001.txt", []byte(inputConten), 0644)
 	if err != nil {
 		t.Fatalf("failed to write input file: %v", err)
 	}
@@ -34,12 +34,12 @@ func TestRun(t *testing.T) {
 	}
 
 	// 出力ファイルの確認
-	outputContent, err := os.ReadFile("testdata/output/0001.out")
+	outputContent, err := os.ReadFile("testdata/0001.out")
 	if err != nil || string(outputContent) != inputConten {
 		t.Fatalf("Unexpected output. Expected: %s, Actual: %s", inputConten, string(outputContent))
 	}
 
 	// 後始末
-	defer os.Remove("testdata/input/0001.txt")
-	defer os.Remove("testdata/output/0001.out")
+	defer os.Remove("testdata/0001.txt")
+	defer os.Remove("testdata/0001.out")
 }
