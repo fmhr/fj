036cf27db9996d933cd8d94d7262a5be4d725da6
fmhr <fmhr29@gmail.com>
2023-09-26 15:05:15 +0900

    BUG Attempted the impossible.

--- fj.go
+++ fj.go
@@ -2,7 +2,9 @@ package fj
 
 import (
 	"flag"
+	"fmt"
 	"log"
+	"os"
 )
 
 func init() {
@@ -10,61 +12,75 @@ func init() {
 }
 
 func Fj() {
-	mode := flag.String("mode", "help", "モードの選択 [init(設定ファイルの生成), test(コマンドの実行), gen(fj -mode gen -seed 1 (シード1のテストwケースw生成) gcloud")
-	seed := flag.Int("seed", 0, "テストケースのシード値を指定。未設定の場合は0 (0000.txt) が選択されます。-end オプションが指定されている場合、このオプションは無視されます。")
-	start := flag.Int("start", 0, "開始シードの指定。デフォルトは０")
-	end := flag.Int("end", 0, "[start, end) のシードケースがテストされる.")
-	cmdArgs := flag.String("cmdArgs", "", "指定されたとき、 `Cmd [cmdArgs]`として実行される")
-	jobs := flag.Int("jobs", -1, "同時実行するwジョブの上限w。。CPUのコア数を越えるとパフォーマンスが低下。")
-	cloud := flag.Bool("cloud", false, "クラウドモード")
+	seed := flag.Int("seed", 0, "テストケースのシード値を指定 未設定の場合は0 (0000.txt) が選択されます")
+	start := flag.Int("start", 0, "開始シードの指定 デフォルト:０")
+	end := flag.Int("end", 10, "終了シード デフォルト:10")
+	cmdArgs := flag.String("cmdArgs", "", "`Cmd [cmdArgs]`として実行される")
+	jobs := flag.Int("jobs", 1, "並列実行数 デフォルト:1")
+	cloud := flag.Bool("cloud", false, "クラウドモード デフォルト:false")
 	flag.Parse()
 
-	switch *mode {
-	case "init":
-		GenerateConfig()
-		return
-	case "test", "gen", "cloud":
-		cnf, err := LoadConfigFile()
-		if err != nil {
-			log.Fatal(err)
-		}
-		if *cloud {
-			cnf.Cloud = true
-		}
-		if *cmdArgs != "" {
-			cnf.Cmd = cnf.Cmd + " " + *cmdArgs
-		}
-		seeds := []int{}
-		if *end > *start {
-			for i := *start; i < *end; i++ {
-				seeds = append(seeds, i)
-			}
-		} else {
-			seeds = append(seeds, *seed)
-		}
-		if *jobs > 0 {
-			cnf.Jobs = *jobs
-		}
-		// modeによって処理を分岐
-		switch *mode {
-		case "test":
-			RunParallel(cnf, seeds)
-		case "gen":
-			Gen(cnf, *seed)
-		case "cloud":
-			log.Println("cloud mode")
-			rtn, err := CloudRun(cnf, *seed)
-			if err != nil {
-				log.Fatal(err)
-			}
-			log.Println(rtn)
-		}
-	default:
-		flag.Usage()
+	if len(flag.Args()) < 1 {
+		fmt.Println("Usage: fj [command] [options]")
+		os.Exit(1)
 	}
 
-	if *mode == "init" {
+	cmd := flag.Args()[0]
+
+	if cmd == "init" {
 		GenerateConfig()
 		return
 	}
+	config, err := LoadConfigFile()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if config.Cmd == "" {
+		fmt.Printf("%s dファイルで実行コマンドを設定してください\n", configFileName)
+		os.Exit(1)
+	}
+
+	// check flags
+	if isFlagSet("cmdArgs") {
+		config.Args = []string{*cmdArgs}
+	}
+	if isFlagSet("jobs") {
+		config.Jobs = *jobs
+	}
+	if isFlagSet("cloud") {
+		config.Cloud = *cloud
+	}
+	log.Println(*seed)
+	switch cmd {
+	case "test":
+		var rtn map[string]float64
+		var err error
+		if config.Reactive {
+			rtn, err = ReactiveRun(config, *seed)
+		} else {
+			rtn, err = RunVis(config, *seed)
+		}
+		if err != nil {
+			log.Fatal("Error: ", err)
+		}
+		fmt.Println(rtn)
+	case "tests":
+		seeds := make([]int, *end-*start)
+		for i := *start; i < *end; i++ {
+			seeds[i-*start] = i
+		}
+		RunParallel(config, seeds)
+	}
+}
+
+func isFlagSet(flagName string) bool {
+	flagSet := false
+
+	flag.Visit(func(f *flag.Flag) {
+		if f.Name == flagName {
+			flagSet = true
+		}
+	})
+	return flagSet
 }
