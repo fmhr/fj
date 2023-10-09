9a03ec93cce139f7df5178bbc4a522c09e0d3169
fmhr <fmhr29@gmail.com>
2023-10-09 19:04:26 +0900

    add sendBinaryToWorker

---
+++ cmd/worker/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"os"
+)
+
+func uploadHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Only POST is allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	// バイナリの受け取り
+	file, _, err := r.FormFile("binary")
+	if err != nil {
+		http.Error(w, "Failed to get the binary", http.StatusBadRequest)
+		return
+	}
+	defer file.Close()
+
+	tmpFile, err := os.CreateTemp("", "uploaded-binary-*")
+	if err != nil {
+		http.Error(w, "Failed to create a temp file", http.StatusInternalServerError)
+		return
+	}
+	tmpFile.Close()
+
+	_, err = io.Copy(tmpFile, file)
+	if err != nil {
+		http.Error(w, "Failed to save the binary", http.StatusInternalServerError)
+		return
+	}
+
+	// language
+	language := r.FormValue("language")
+	if language == "" {
+		http.Error(w, "Language not specified", http.StatusBadRequest)
+		return
+	}
+	// TODO:ここに処理
+
+	w.Write([]byte("Successfully uploaded and processed"))
+}
+
+func main() {
+	http.HandleFunc("/upload", uploadHandler)
+	http.ListenAndServe(":8080", nil)
+}
---
+++ sendBinaryToWorker.go
@@ -0,0 +1,62 @@
+package fj
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
+func sendBinaryToWorker(workerURL, binaryPath, language string) (string, error) {
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+
+	// バイナリの追加
+	file, err := os.Open(binaryPath)
+	if err != nil {
+		return "", fmt.Errorf("failed to open binary file %s: %v", binaryPath, err)
+	}
+	defer file.Close()
+
+	part, err := writer.CreateFormFile("binary", filepath.Base(binaryPath))
+	if err != nil {
+		return "", fmt.Errorf("failed to create form file for binary: %v", err)
+	}
+
+	_, err = io.Copy(part, file)
+	if err != nil {
+		return "", fmt.Errorf("failed to write binary to form file: %v", err)
+	}
+
+	// language
+	writer.WriteField("langiage", language)
+	writer.Close()
+
+	// リクエストの送信
+	req, err := http.NewRequest("POST", workerURL, body)
+	if err != nil {
+		return "", fmt.Errorf("failed to create HTTP request: %v", err)
+	}
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", fmt.Errorf("failed to send  HTTP request to worker: %v", err)
+	}
+	defer resp.Body.Close()
+
+	// レスポンスボディから文字列を取り出す
+	bodyBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", fmt.Errorf("failed to read response body: %v", err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("worker returned an unexpected status: %v", resp.Status)
+	}
+	return string(bodyBytes), nil
+}
