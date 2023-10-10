639038b263dba8a68a258060db1be78517535a96
fmhr <fmhr29@gmail.com>
2023-10-11 01:00:35 +0900

    remove file

--- googleCloud.go
+++
@@ -1,6 +0,0 @@
-package fj
-
-// すでにバイナリがある状態にしておく
-func requestGoogleCloud(config *Config, seed int) (map[string]float64, error) {
-	return sendBinaryToWorker(config.WorkerURL, config.BinaryPath, config.Language, seed)
-}
