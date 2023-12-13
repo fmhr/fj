2d633d28fd42f676b0a7e8d27bb3b711da217c23
fmhr <fmhr29@gmail.com>
2023-12-13 19:44:33 +0900

    remove unused cloudbuild.yaml

--- builds/go/submit.sh
+++
@@ -1,8 +0,0 @@
-# Cloud Buildをトリガーし、その出力を変数に保存
-OUTPUT=$(gcloud builds submit --config cloudbuild.yaml .)
-
-# 出力からBUILD_IDを抽出
-BUILD_ID=$(echo "$OUTPUT" | grep "^ID" -A 1 | tail -n1 | awk '{print $1}')
-# BUILD_IDを表示
-echo $BUILD_ID
-
