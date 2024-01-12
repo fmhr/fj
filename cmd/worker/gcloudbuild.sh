85cad4f5b05a67b07faeb8aaa7eff84120b03806
fmhr <fmhr29@gmail.com>
2024-01-13 02:36:28 +0900

    Update Dockerfile and gcloudbuild.sh

--- cmd/worker/gcloudbuild.sh
+++ cmd/worker/gcloudbuild.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 GCLOUD_PROJECT="project2323"
-GCLOUD_IMAGE_TAG="fj-worker"
+IMAGE_NAME="fj-worker"
 
 set -e #コマンドが失敗したらそこで終了する
 set -x #実行したコマンドを表示する
 
-gcloud builds submit --tag gcr.io/$GCLOUD_PROJECT/$GCLOUD_IMAGE_TAG .
+gcloud builds submit --tag gcr.io/$GCLOUD_PROJECT/$IMAGE_NAME .
 gcloud run deploy worker\
-    --image gcr.io/$GCLOUD_PROJECT/$GCLOUD_IMAGE_TAG \
+    --image gcr.io/$GCLOUD_PROJECT/$IMAGE_NAME \
     --platform managed --allow-unauthenticated \
     --concurrency 1
