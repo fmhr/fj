bfddc659c98a3eb24014cabbc8134c8d31926f5c
fmhr <fmhr29@gmail.com>
2024-03-05 11:19:55 +0900

    unix_specific.goとwindows_specific.goを追加し、runCommandWithTimeout関数をそれぞれのOSに合わせて修正

---
+++ unix_specific.go
@@ -0,0 +1,51 @@
+//go:build linux || darwin
+// +build linux darwin
+
+package fj
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"os/exec"
+	"syscall"
+	"time"
+)
+
+func runCommandWithTimeout(cmdStrings []string, cnf *Config) ([]byte, string, error) {
+	var result string
+	cmd := exec.Command(cmdStrings[0], cmdStrings[1:]...)
+	cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
+	var stdoutBuf, stderrBuf bytes.Buffer
+	cmd.Stdout = &stdoutBuf
+	cmd.Stderr = &stderrBuf
+	if err := cmd.Start(); err != nil {
+		return nil, result, fmt.Errorf("cmd.Start() failed with: %v", err)
+	}
+	errCh := make(chan error, 1)
+	go func() {
+		errCh <- cmd.Wait()
+		close(errCh)
+	}()
+	var err error
+	select {
+	case <-time.After(time.Duration(cnf.TimeLimitMS) * time.Millisecond):
+		if cmd.Process != nil {
+			syscall.Kill(-cmd.Process.Pid, syscall.SIGKILL) // -pgidで子プロセスもkill
+			//cmd.Process.Kill()
+			result = "TLE"
+		}
+	case err := <-errCh:
+		if err != nil {
+			log.Println("Error: ", err, "command:", cmd.String())
+			return nil, result, fmt.Errorf("cmd.Wait() failed with: %v", err)
+		}
+	}
+
+	// タイムアウトして、-race をつけたときに、WARNING: DATA RACE がでるのは避けられない
+	// https://github.com/golang/go/issues/22757
+	rtn := make([]byte, 0, len(stdoutBuf.Bytes())+len(stderrBuf.Bytes()))
+	rtn = append(rtn, stdoutBuf.Bytes()...)
+	rtn = append(rtn, stderrBuf.Bytes()...)
+	return rtn, result, err
+}
