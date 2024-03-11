72d40b42c72d5b4effdd6de1e31a0a40ee89aaa4
fmhr <fmhr29@gmail.com>
2024-03-11 19:32:18 +0900

    fix

--- languageSet.go
+++ languageSet.go
@@ -23,7 +23,7 @@ var LanguageSets = []LanguageSet{
 	{
 		Language:   "cpp",
 		FileName:   "main.cpp",
-		CompileCmd: "g++-12 -std=gnu++20 -O2 -DONLINE_JUDGE -DATCODER -Wall -Wextra -mtune=native -march=native -fconstexpr-depth=2147483647 -fconstexpr-loop-limit=2147483647 -fconstexpr-ops-limit=2147483647 -l/opt/ac-library -l/opt/boost/gcc/include -L/opt/boost/gcc/lib -o a.out Main.cpp -lgmpxx -lgmp -l/usr/include/eigen3",
+		CompileCmd: "g++-12 -std=gnu++20 -O2 -DONLINE_JUDGE -DATCODER -Wall -Wextra -mtune=native -march=native -fconstexpr-depth=2147483647 -fconstexpr-loop-limit=2147483647 -fconstexpr-ops-limit=2147483647 -I/opt/ac-library -I/opt/boost/gcc/include -L/opt/boost/gcc/lib -o a.out Main.cpp -lgmpxx -lgmp -L/usr/include/eigen3",
 		ExeCmd:     "./a.out",
 	},
 	{
