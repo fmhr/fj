9cdd4abb5699dd8fbffe171fc8f6fb4cd495b259
fmhr <fmhr29@gmail.com>
2024-03-11 18:47:50 +0900

    add languageSet

---
+++ languageSet.go
@@ -0,0 +1,41 @@
+package fj
+
+type LanguageSet struct {
+	Language   string
+	FileName   string
+	CompileCmd string
+	ExeCmd     string
+}
+
+var LanguageSets = []LanguageSet{
+	{
+		Language:   "go",
+		FileName:   "main.go",
+		CompileCmd: "go build -o a.out main.go",
+		ExeCmd:     "./a.out",
+	},
+	{
+		Language:   "rust",
+		FileName:   "main.rs",
+		CompileCmd: "cargo build --release --quiet --offline",
+		ExeCmd:     "./a.out",
+	},
+	{
+		Language:   "cpp",
+		FileName:   "main.cpp",
+		CompileCmd: "g++-12 -std=gnu++20 -O2 -DONLINE_JUDGE -DATCODER -Wall -Wextra -mtune=native -march=native -fconstexpr-depth=2147483647 -fconstexpr-loop-limit=2147483647 -fconstexpr-ops-limit=2147483647 -l/opt/ac-library -l/opt/boost/gcc/include -L/opt/boost/gcc/lib -o a.out Main.cpp -lgmpxx -lgmp -l/usr/include/eigen3",
+		ExeCmd:     "./a.out",
+	},
+	{
+		Language:   "java",
+		FileName:   "Main.java",
+		CompileCmd: "javac -Xss128M -DONLINE_JUDGE=true Main",
+		ExeCmd:     "java Main",
+	},
+	{
+		Language:   "cs",
+		FileName:   "publish/Main",
+		CompileCmd: "export DOTNET_EnableWriteXorExecute=0 && dotnet publish -c Release -o publish -v q --nologo 1>&2",
+		ExeCmd:     "./publish/Main",
+	},
+}
