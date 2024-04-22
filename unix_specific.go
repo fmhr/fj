4b85ef5aa0f7a076261841952b0d1bbef4b16d2e
fmhr <fmhr29@gmail.com>
2024-04-23 04:42:23 +0900

    リファクタリング

--- unix_specific.go
+++ unix_specific.go
@@ -12,44 +12,51 @@ import (
 	"time"
 )
 
-// runCommandWithTimeout はコマンドを実行する　時間内に終わらなかったらSIGKILLを送る
-// cnfから使っているのはTimeLimitMSだけ
+// runCommandWithTimeout は指定されたタイムアウトでコマンドを実行し、標準出力と標準エラー出力の結合された内容と結果文字列、およびエラーを返す
 func runCommandWithTimeout(cmdStrings []string, timelimitMS int) ([]byte, string, error) {
-	var result string
+	if len(cmdStrings) == 0 {
+		return nil, "", fmt.Errorf("cmdStrings must not be empty")
+	}
+
 	cmd := exec.Command(cmdStrings[0], cmdStrings[1:]...)
-	cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
+	cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true} // -pgidで子プロセスもkillできるようにする
+
 	var stdoutBuf, stderrBuf bytes.Buffer
 	cmd.Stdout = &stdoutBuf
 	cmd.Stderr = &stderrBuf
+
 	if err := cmd.Start(); err != nil {
-		return nil, result, fmt.Errorf("cmd.Start() failed with: %v", err)
+		return nil, "", fmt.Errorf("cmd.Start() failed with: %v", err)
 	}
+
 	errCh := make(chan error, 1)
 	go func() {
 		errCh <- cmd.Wait()
 		close(errCh)
 	}()
-	var err error
+
+	timedOut := false
 	select {
 	case <-time.After(time.Duration(timelimitMS) * time.Millisecond):
 		if cmd.Process != nil {
 			syscall.Kill(-cmd.Process.Pid, syscall.SIGKILL) // -pgidで子プロセスもkill
 			//cmd.Process.Kill()
-			result = "TLE"
 		}
+		timedOut = true
 	case err := <-errCh:
 		if err != nil {
 			rtn := make([]byte, 0, len(stdoutBuf.Bytes())+len(stderrBuf.Bytes()))
 			rtn = append(rtn, stdoutBuf.Bytes()...)
 			rtn = append(rtn, stderrBuf.Bytes()...)
 			log.Println("Error: ", err, "command:", cmd.String())
-			return rtn, result, fmt.Errorf("cmd.Wait() failed with: %v", err)
+			return rtn, "", fmt.Errorf("cmd.Wait() failed with: %v", err)
 		}
 	}
-	// タイムアウトして、-race をつけたときに、WARNING: DATA RACE がでるのは避けられない
-	// https://github.com/golang/go/issues/22757
-	rtn := make([]byte, 0, len(stdoutBuf.Bytes())+len(stderrBuf.Bytes()))
-	rtn = append(rtn, stdoutBuf.Bytes()...)
-	rtn = append(rtn, stderrBuf.Bytes()...)
-	return rtn, result, err
+	output := append(stdoutBuf.Bytes(), stderrBuf.Bytes()...)
+
+	if timedOut {
+		return output, "TLE", nil
+	}
+
+	return output, "Sccess", nil
 }
