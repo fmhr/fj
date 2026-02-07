4367756e457e9b82f10fb8b6fddaff6a7c6e0a34
fmhr <fmhr29@gmail.com>
2026-02-07 22:02:58 +0900

    エラーハンドリングを改善し、詳細なエラーメッセージをfmt.Errorfを使用して追加

--- cmd/gen.go
+++ cmd/gen.go
@@ -11,7 +11,7 @@ import (
 
 func Gen(cnf *setup.Config, seed int) error {
 	if cnf.GenPath == "" {
-		return NewStackTraceError("GenPath is not set. please set GenPath: {} in fj/config.toml")
+		return fmt.Errorf("GenPath is not set. please set GenPath: {} to fj/config.toml")
 	}
 	return gen(cnf, seed)
 }
@@ -29,21 +29,21 @@ func gen(cnf *setup.Config, seed int) error {
 	if _, err := os.Stat(path); os.IsNotExist(err) {
 		err := os.Mkdir(path, 0755)
 		if err != nil {
-			return WrapError(err)
+			return fmt.Errorf("in2ディレクトリの作成に失敗: %w", err)
 		}
 	} else if err != nil {
-		return WrapError(err)
+		return fmt.Errorf("in2ディレクトリの確認に失敗: %w", err)
 	}
 	// genがあるか確認
 	_, err := os.Stat(cnf.GenPath)
 	if err != nil {
-		return WrapError(err)
+		return fmt.Errorf("genファイルの確認に失敗: %w", err)
 	}
 	// seedを書き込んだ{seed}.txtを作成
 	seedfile := "seed.txt"
 	err = writeIntToFile(seed, seedfile)
 	if err != nil {
-		return WrapError(err)
+		return fmt.Errorf("シードファイルの作成に失敗: %w", err)
 	}
 	// genを実行
 	cmdStr := fmt.Sprintf("%s %s", cnf.GenPath, seedfile)
@@ -51,16 +51,20 @@ func gen(cnf *setup.Config, seed int) error {
 	cmd := exec.Command(cmdStrings[0], cmdStrings[1:]...)
 	err = cmd.Run()
 	if err != nil {
-		return WrapError(err)
+		return fmt.Errorf("genの実行に失敗: %w", err)
 	}
 	// in/0000.txtをin/{seed}.txtにリネーム
 	err = os.Rename("in/0000.txt", fmt.Sprintf("in2/%04d.txt", seed))
 	if err != nil {
-		return WrapError(err)
+		return fmt.Errorf("inファイルのリネームに失敗: %w", err)
 	}
 	// cnf.InfilePathを更新
 	cnf.InfilePath = "in2/"
 	// (seed.txt)を削除
+	err = os.Remove("seed.txt")
+	if err != nil {
+		return fmt.Errorf("シードファイルの削除に失敗: %w", err)
+	}
 	return nil
 }
 
