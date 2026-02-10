24536e2d578082946777e575e14cf8916b2343a1
fmhr <fmhr29@gmail.com>
2026-02-10 22:27:10 +0900

    IMAGE_NAMEが設定されていない場合のエラーメッセージを追加し、スクリプトの実行を停止するように変更

--- cmd/worker/script/worker_build.sh
+++ cmd/worker/script/worker_build.sh
@@ -8,7 +8,10 @@ REGION="asia-northeast1"
 set -e #コマンドが失敗したらそこで終了する
 set -x #実行したコマンドを表示する
 
-
+if [ -z "$IMAGE_NAME" ]; then
+  echo "IMAGE_NAME is not set. e.g., IMAGE_NAME=ahc001a"
+  exit 1    
+fi
 
 gcloud builds submit --tag ${REGION}-docker.pkg.dev/${GCLOUD_PROJECT}/${REPOSITORY}/${IMAGE_NAME}:latest .
 
